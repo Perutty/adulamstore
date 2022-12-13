2c84983211c46f41057d140a2d2a6f3b31117885
Perutty <brayanalexanderpera@ufps.edu.co>
2022-12-13 17:47:02 -0500

    ajustes adicionales al archivo cart.js

--- src/main/resources/static/js2/cart.js
+++ src/main/resources/static/js2/cart.js
@@ -9,26 +9,18 @@ const pagarCart = document.querySelector('#pagar');
 const activar = document.querySelector('#activar');
 const borrarP = document.querySelector('.detalleProductos');
 const totalPagar = document.querySelector('.total');
+const containerDetails = document.querySelector('#tablax tbody');
 
 
 //Variable de arreglo de productos
 let allProducts = [];
 
-if(activar){
+if(activar || cartInfo){
 document.addEventListener('DOMContentLoaded', () => {
 		if(localStorage.getItem('allProducts')){
        		 allProducts = JSON.parse(localStorage.getItem('allProducts'));
        	 	 showDetails();
        	 	 showTotal();
-       	 	 showHTML();
-        }
-});
-}
-
-if(cartInfo){
-document.addEventListener('DOMContentLoaded', () => {
-		if(localStorage.getItem('allProducts')){
-       		 allProducts = JSON.parse(localStorage.getItem('allProducts'));
        	 	 showHTML();
         }
 });
@@ -55,45 +47,50 @@ if(pagar){
 
 if(borrarP){
 	borrarP.addEventListener('click', e =>{
-		if(e.target.classList.contains('borrar')){
+		if(e.target.classList.contains('delete')){
 		
-			const product = e.target.parentElement.parentElement.parentElement;
-			const title = product.querySelector('tr').textContent;
+			const product = e.target.parentElement.parentElement;
+			const title = product.querySelector('#na').textContent;
 		
 			allProducts = allProducts.filter(product => product.title !== title);
 			showDetails();
+			showTotal();
+			showHTML();
 		}
+			if(totalOfProducts === 0){
+				allProducts.length = 0;
+				localStorage.clear();
+			}
 	});
 }
 
-
 function showDetails (){
+	//limpíarHTML
+	containerDetails.innerHTML= '';
 	allProducts.forEach(product => {
-		const containerDetails = document.querySelector('#tablax tbody');
 		if(containerDetails){
 			const row =  document.createElement('tr');
 			row.innerHTML += `
-							 <td style="width:20%"><img src="${product.img}" style="width:30%"/></td>
-							 <td>${product.title}</td>
+							 <td style="width:15%"><img src="${product.img}" style="width:70%"/></td>
+							 <td id="na">${product.title}</td>
 							 <td>${product.quantity}</td>
 							 <td>$ ${parseInt(product.price.slice(1)*product.quantity)}</td>
 							 <td>
-								 <button class="borrar"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
+								 <button class="delete"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
 								 </button>
 							 </td>
 						  
 		`;
 	  		containerDetails.appendChild(row);
 	  	}
-	});
+	});	
 }
 
 
 if(productsList){
-productsList.addEventListener('click', e => {
+	productsList.addEventListener('click', e => {
 		if(e.target.classList.contains('add-to-cart-btn')){
 			const product = e.target.parentElement.parentElement;
-		
 			const infoProduct = {
 				img: product.querySelector('p').textContent,
 				quantity: 1,
@@ -113,7 +110,7 @@ productsList.addEventListener('click', e => {
 				allProducts = [...products];
 			}else{
 				
-			allProducts = [...allProducts, infoProduct];
+				allProducts = [...allProducts, infoProduct];
 			}
 			showHTML();
 		}
@@ -135,7 +132,7 @@ if(rowProduct){
 }
 
 //Funcion para mostrar HTML
-const showHTML = () => {
+function showHTML(){
 	
 	//limpíarHTML
 	rowProduct.innerHTML= '';
@@ -166,11 +163,11 @@ const showHTML = () => {
 		countProduct.innerText = totalOfProducts;
 };
 
-const showTotal =() => {
+function showTotal (){
+	totalPagar.innerHTML='';
 	let pagar = 0;
 	allProducts.forEach(product => {
 		pagar = pagar+parseInt(product.quantity * product.price.slice(1));;
 		totalPagar.innerText = `El total a pagar es: $ ${pagar}`;
-		console.log(pagar);
 	});
 }
